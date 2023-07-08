5dc4523b63cd247acd7f86b551c32f28b83ed1c9
Asliddin Isroilov <57828415+codeBroh@users.noreply.github.com>
2023-07-08 17:56:40 +0300

    migrathions

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.utils.timezone import now
 class Foydalanuvchi(models.Model):
     ism = models.CharField(max_length=25)
     familiya = models.CharField(max_length=25)
     pochta = models.EmailField()
+    dob = models.DateTimeField(default=now)
 
     def __str__(self):
         return f"{self.ism} {self.familiya}"
@@ -16,4 +17,4 @@ class Vazifalar(models.Model):
     bajarildi = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.sarlavha
