2663aac22ce3b9b6131bbda3c3da1f3f65a370c5
Asliddin Isroilov <57828415+codeBroh@users.noreply.github.com>
2023-07-08 17:15:30 +0300

    translated

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Vazifalar
 # CRUD
 
 def create(request):
     return HttpResponse("Add a new one")
 
 def read(request): #list of activities
-    return HttpResponse("Your list of activities")
+    model = Vazifalar.objects.all().values()
+    context = {"nomi": model}
+    return render(request, "home.html", context)
 
 def update(request):
     return HttpResponse("Mark your activity")
