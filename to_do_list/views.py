ba05d8e2c0abc2433ca704e5a625980ead610b9d
Asliddin Isroilov <57828415+codeBroh@users.noreply.github.com>
2023-07-29 23:41:11 +0300

    .

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-import pytz
 from datetime import date, datetime
 from django.db.models import Q
 
