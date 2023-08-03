24afb1ee292ed062302281bb52ff3790f518aa0b
Asliddin Isroilov <57828415+codeBroh@users.noreply.github.com>
2023-08-03 13:21:02 +0300

    profile frontend, labels, etc.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path("deleteFoydalanuvchi/", views.deleteFoydalanuvchi, name="deleteFoydalanuvchi"),
     path("changeParolFoydalanuvchi/", views.changeParolFoydalanuvchi, name="changeParolFoydalanuvchi"),
     path('changeParolFoydalanuvchi/changeParolDone/', PasswordChangeDoneView.as_view(template_name='accounts/changeParolDone.html'), name='changeParolDone'),
+    path('changeProfile', views.changeProfile, name="changeProfile"),
     path("editFoydalanuvchi/<int:idMe/", views.editFoydalanuvchi, name="editFoydalanuvchi"),
     path("profileFoydalanuvchi/", views.profileFoydalanuvchi, name="profileFoydalanuvchi"),
     path("registerFoydalanuvchi/", views.registerFoydalanuvchi, name="registerFoydalanuvchi"),
